6fa8cbddffee60eb445f8a77a8fa5ffbfecdf80b
tisho <albertisho99@gmail.com>
2020-03-22 19:23:12 +0100

    Modificación E-R + ORM Django

--- PlayStackCharm/playstackDjango/playstackDjango/App/PlayStack/models.py
+++ PlayStackCharm/playstackDjango/playstackDjango/App/PlayStack/models.py
@@ -4,14 +4,14 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-    Nombre = models.CharField(max_length=25, primary_key=True)
+    NombreUsuario = models.CharField(max_length=25, unique=True)
     Contrasenya = models.CharField(max_length=50, null=False)
-    Correo = models.CharField(max_length=100)
+    Correo = models.CharField(max_length=100, null=False)
     FotoDePerfil = models.ImageField()
     Seguidos = models.ManyToManyField('self', blank=True)
 
     def __str__(self):
-        return self.Nombre
+        return self.NombreUsuario
 
 
 class Premium(models.Model):
@@ -24,7 +24,7 @@ class Premium(models.Model):
 
 class NoPremium(models.Model):
     Nombre = models.OneToOneField(Usuario, null=False, blank=False, on_delete=models.CASCADE,
-                                  parent_link=True)
+                                  parent_link=True)  # WARNING: Es posible que falte la reestriccion de max y min
     NumSalt = models.IntegerField()
 
     def __str__(self):
@@ -32,16 +32,16 @@ class NoPremium(models.Model):
 
 
 class CreadorContenido(models.Model):
-    Nombre = models.OneToOneField(Usuario, null=False, blank=False, on_delete=models.CASCADE,
-                                  parent_link=True)
+    NombreCreador = models.OneToOneField(Usuario, null=False, blank=False, on_delete=models.CASCADE,
+                                         parent_link=True)
 
     def __str__(self):
-        return self.Nombre
+        return self.NombreCreador
 
 
 class Audio(models.Model):
-    IDAudio = models.IntegerField(primary_key=True)
-    FicheroDeAudio = models.FileField(null=False)
+    ID = models.IntegerField(primary_key=True)
+    FicheroAudio = models.FileField(null=False)
     Titulo = models.CharField(max_length=30, null=False)
     Idioma = models.CharField(max_length=15, null=False)
     Duracion = models.DecimalField(max_digits=5, decimal_places=2, null=False)
@@ -50,37 +50,10 @@ class Audio(models.Model):
     UsuariosComoFavorita = models.ManyToManyField(Usuario, blank=True, related_name='Audios')
 
     class Meta:
-        unique_together = ('IDAudio', 'CreadorDeContenido')
+        unique_together = ('ID', 'CreadorDeContenido')
 
     def __str__(self):
         return self.Titulo
-
-
-class Artista(models.Model):
-    Nombre = models.CharField(max_length=30, primary_key=True)
-    PaisDeNacimiento = models.CharField(max_length=15)
-    Canciones = models.ManyToManyField(Audio, blank=False, related_name='Artistas')
-
-    def __str__(self):
-        return self.Nombre
-
-
-class Genero(models.Model):
-    ID = models.IntegerField(primary_key=True)
-    Nombre = models.CharField(max_length=1000, null=False)
-    Canciones = models.ManyToManyField(Audio, blank=False, related_name='Generos')
-
-    def __str__(self):
-        return self.Nombre
-
-
-class Podcast(models.Model):
-    ID = models.OneToOneField(Audio, null=False, blank=False, on_delete=models.CASCADE,
-                              parent_link=True)
-    Resumen = models.CharField(max_length=1000, null=False)
-
-    def __str__(self):
-        return self.ID
 
 
 class Cancion(models.Model):
@@ -91,22 +64,10 @@ class Cancion(models.Model):
         return self.ID
 
 
-class PlayList(models.Model):
-    IDPlayList = models.IntegerField(primary_key=True)
-    Nombre = models.CharField(max_length=30, null=False)
-    UsuarioNombre = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('IDPlayList', 'UsuarioNombre')
-
-    def __str__(self):
-        return self.Nombre
-
-
-class Carpeta(models.Model):
-    ID = models.IntegerField(primary_key=True)
-    Nombre = models.CharField(max_length=30, null=False)
-    PlayList = models.ManyToManyField(PlayList, blank=True, related_name='Carpetas')
+class Artista(models.Model):
+    Nombre = models.CharField(max_length=30, unique=True)
+    PaisDeNacimiento = models.CharField(max_length=30)
+    Canciones = models.ManyToManyField(Audio, blank=True, related_name='Artistas')
 
     def __str__(self):
         return self.Nombre
@@ -114,8 +75,64 @@ class Carpeta(models.Model):
 
 class Album(models.Model):
     ID = models.IntegerField(primary_key=True)
-    NombreAlbum = models.CharField(max_length=1000, null=False)
-    Canciones = models.ManyToManyField(Audio, blank=True, related_name='Albunes')
+    Nombre = models.CharField(max_length=100, null=False)
+    Canciones = models.ManyToManyField(Audio, blank=False, related_name='Albunes')
 
     def __str__(self):
-        return self.NombreAlbum
+        return self.Nombre
+
+
+class Genero(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=100, null=False)
+    Canciones = models.ManyToManyField(Audio, blank=True, related_name='Generos')
+
+    def __str__(self):
+        return self.Nombre
+
+
+class PlayList(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=30, null=False)
+    Privado = models.BooleanField(null=False)
+    UsuarioNombre = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('ID', 'UsuarioNombre')
+
+    def __str__(self):
+        return self.Nombre
+
+
+class Carpeta(models.Model):
+    # ID = models.IntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=30, null=False)
+    PlayList = models.ManyToManyField(PlayList, blank=True, related_name='Carpetas')
+
+    def __str__(self):
+        return self.Nombre
+
+
+class Capitulo(models.Model):
+    ID = models.OneToOneField(Audio, null=False, blank=False, on_delete=models.CASCADE,
+                              parent_link=True)
+
+    def __str__(self):
+        return self.ID
+
+
+class Podcast(models.Model):
+    Nombre = models.CharField(max_length=50, null=False)
+    Descripcion = models.TextField(null=False)
+    Subscriptores = models.ManyToManyField(Usuario, blank=True, related_name='Suscrito')
+    Capitulos = models.ForeignKey(Capitulo, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Nombre
+
+class Interlocutor(models.Model):
+    Nombre = models.CharField(max_length=50, null=False)
+    Podcasts = models.ManyToManyField(Podcast, blank=True, related_name='Participan')
+
+    def __str__(self):
+        return self.Nombre
